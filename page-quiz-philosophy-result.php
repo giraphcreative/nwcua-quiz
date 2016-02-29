3a9d3267ba7ff9cf063fc914a4b3015453bc8978
James Pederson <james@jpederson.com>
2016-02-29 13:50:19 -0600

    Correct logic for testing if they passed.

--- page-quiz-philosophy-result.php
+++ page-quiz-philosophy-result.php
@@ -16,7 +16,7 @@ $text_questions = 1;
 			$entry = GFAPI::get_entry( $_REQUEST['id'] );
 			$upload_dir = wp_upload_dir();
 			$total_questions = ceil( $entry['gquiz_score'] / ( $entry['gquiz_percent'] / 100 ) ) + $text_questions;
-			if ( $entry['gquiz_score'] ) {
+			if ( $entry['gquiz_is_pass'] ) {
 				?>
 		<h2>Congratulations on completing the NWCUA’s Credit Union Philosophy Assessment!</h2>
 		<h1>You answered <?php print $entry['gquiz_score'] + $text_questions ?> out of <?php print $total_questions ?> correctly.</h1>
